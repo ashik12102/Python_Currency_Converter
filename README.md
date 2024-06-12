7d95d332675b3d12b2ff282ca6e7e968d2da1e2c
Mohamed Ashik <ashik12102@gmail.com>
2024-06-12 18:38:43 +0200

    made changeds

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ Created three python file
     Currency_validation -  This will check if the currency is valid
     Amount_validation - this will check if the amount entered is valid
     main - this will be the main file where the project is executed
+
+
+created allinone file
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import requests
 import Currency_validation
 import Amount_validation
 
+
+url = f"https://api.apilayer.com/fixer/convert?to={to}&from={from}&amount={amount}"
+
+payload = {}
+headers= {
+  "apikey": "sYOvFbbasSjMOuH7UK27PNcwpOohXlkw"
+  }
+response = requests.request("GET", url, headers=headers, data = payload)
+print(response.json())
 # Run this file for test
